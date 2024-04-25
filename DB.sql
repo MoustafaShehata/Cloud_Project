8d548f6d32cde98223f5485001de49c3e91442f2
Moustafa Shehata <167570674+MoustafaShehata@users.noreply.github.com>
2024-04-25 19:01:51 +0200

    Add files via upload

---
+++ DB.sql
@@ -0,0 +1,19 @@
+/* Code To Create Table
+CREATE Table Student_Data 
+(
+StudentName VARCHAR(50),
+Id INT ,
+Age INT,
+CGPA FLOAT
+)
+*/
+/* Code To Insert The Data
+INSERT INTO Student_Data VALUES
+('Ahmed',22010036,20,3.3),
+('Khaled',22010087,20,2.9),
+('Hazem',22010328,20,3.375),
+('Islam',22010049,20,2.88),
+('Moustafa',22010259,20,3.557)
+*/
+SELECT *
+FROM Student_Data
